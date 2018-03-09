e6a33943557541dc58713dcb876ccc4db85aa45f
randy-darsh <rspringer47@gmail.com>
2018-03-09 11:30:21 -0700

    admin form and display

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import BigFootReport
 
-# Register your models here.
+# admin.site.register(BigFootReport)
+class BigFootReportAdmin(admin.ModelAdmin):
+  list_display = ('report_number', 'report_class', 'county', 'season', 'location_details',
+                  'other_witnesses', 'year', 'state', 'observed', 'time_and_conditions',
+                  'also_noticed', 'other_stories', 'nearest_town', 'nearest_road',
+                  'environment', 'month', 'date', 'a_g_references')
+  fields = ['report_number', 'report_class', 'county', 'season', 'location_details',
+                  'other_witnesses', 'year', 'state', 'observed', 'time_and_conditions',
+                  'also_noticed', 'other_stories', 'nearest_town', 'nearest_road',
+                  'environment', 'month', 'date', 'a_g_references']
+
+admin.site.register(BigFootReport, BigFootReportAdmin)
+
