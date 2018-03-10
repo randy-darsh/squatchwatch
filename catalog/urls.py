8e0e66a97b8725970b73743d6ea13ec3109b3b6a
randy-darsh <rspringer47@gmail.com>
2018-03-10 16:51:29 -0700

    naming issue

--- catalog/urls.py
+++ catalog/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
   path('', views.index, name='index'),
   path('bigfootreports/', views.BigFootReportListView.as_view(), name="bigfootreports"),
-  path('bigfootreports/<int:pk>', views.BigFootReportDetailView.as_view(), name='report-details'),
+  path('bigfootreport/<int:pk>', views.BigFootReportDetailView.as_view(), name='report-detail'),
 ]
