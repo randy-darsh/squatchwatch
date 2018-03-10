d42f70e4166c34569e673e061952ec896e500c4f
randy-darsh <rspringer47@gmail.com>
2018-03-10 14:53:09 -0700

    routes

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
-  path('', views.index, name='index')
+  path('', views.index, name='index'),
+  path('bigfootreports/', views.BigFootReportListView.as_view(), name="bigfootreports"),
+  path('bigfootreports/<int:pk>', views.BigFootReportDetailView.as_view(), name='report-details'),
 ]
