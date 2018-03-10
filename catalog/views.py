72866a0f463ac291753bb431a9ca3c51a10c31ac
randy-darsh <rspringer47@gmail.com>
2018-03-10 14:54:46 -0700

    detail view and list view for reports and linked model to views

--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,18 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import BigFootReport
 
 def index(request):
   return render( request, 'index.html' )
+
+class BigFootReportListView(generic.ListView):
+  model = BigFootReport
+
+class BigFootReportDetailView(generic.DetailView):
+  model = BigFootReport
+
+  def get_context_data(self, **kwargs):
+        context = super(BigFootReportListView, self).get_context_data(**kwargs)
+        # Create any data and add it to the context
+        context['some_data'] = 'This is just some data'
+        return context
