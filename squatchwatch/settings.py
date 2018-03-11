c7c106ccc4e2f74a93e828c7ee1e201e5cfca6b2
randy-darsh <rspringer47@gmail.com>
2018-03-11 01:01:38 -0700

    i am dumb

--- squatchwatch/settings.py
+++ squatchwatch/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'catalog.apps.CatalogConfig',
-    'import-export',
+    'import_export',
 ]
 
 MIDDLEWARE = [
