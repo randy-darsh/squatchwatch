715188b0b3e11a38e842d728a7ea7bd3212249b1
randy-darsh <rspringer47@gmail.com>
2018-03-10 21:26:42 -0700

    settings for importing csvs

--- catalog/admin.py
+++ catalog/admin.py
@@ -14,3 +14,20 @@ class BigFootReportAdmin(admin.ModelAdmin):
 
 admin.site.register(BigFootReport, BigFootReportAdmin)
 
+import tablib
+from import_export import resources
+from core.models import BigFootReport
+bigfootreport = resources.modelresource_factory(model=BigFootReport)()
+dataset = tablib.Dataset(['', 'New Report'], headers=['id', 'report_number', 'report_class', 'county', 'season', 'location_details',
+                                                      'other_witnesses', 'year', 'state', 'observed', 'time_and_conditions',
+                                                      'also_noticed', 'other_stories', 'nearest_town', 'nearest_road',
+                                                      'environment', 'month', 'date', 'a_g_references'])
+result = bigfootreport_resource.import_data(dataset, dry_run=True)
+print result.has_errors()
+result = bigfootreport_resource.import_data(dataset, dry_run=False)
+
+class BigFootReportResource(resources.ModelResource):
+
+  class Meta:
+    model = BigFootReport
+
